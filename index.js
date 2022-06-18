cf0e2cd57b692a6b16ee2dead38dfaf3617625a0
jakiasultana2022 <rokeyasbd@gmail.com>
2022-06-18 21:16:10 +0600

    parts

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
 const express = require('express');
 const cors = require('cors');
-
 require('dotenv').config();
-const app = express()
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
+const app = express();
+const port = process.env.PORT || 8080;
+
+ 
+// middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4wa67.mongodb.net/?retryWrites=true&w=majority`
+
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run (){
+
+    try{
+        await client.connect();
+        console.log('database connected');
+
+        const partCollection = client.db('manufacturer-website').collection('parts');
+
+app.get('/parts',async(req, res)=>{
+  const query = {};
+  const cursor = partCollection.find(query);
+  const parts = await cursor.toArray();
+  res.send(parts) ;
+});
+    
+        
+    }
+
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello from manufacturer website!')
