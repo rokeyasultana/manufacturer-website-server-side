c4e1c74d41470a3f48ed37a5cd657ace0298bbf5
jakiasultana2022 <rokeyasbd@gmail.com>
2022-06-22 10:41:39 +0600

    verify-jwt

--- index.js
+++ index.js
@@ -18,6 +18,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// verify jwt
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+      return res.status(401).send({ message: 'UnAuthorized access' });
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+      if (err) {
+          return res.status(403).send({ message: 'Forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+  });
+}
+
 
 
 
@@ -61,6 +77,7 @@ app.get('/part/:id',async(req,res)=>{
 });
 
 
+
 // users
 app.get('/user',  async (req, res) => {
   const users = await userCollection.find().toArray();
@@ -77,7 +94,7 @@ app.put('/user/:email', async (req, res) => {
       $set: user,
   };
   const result = await userCollection.updateOne(filter, updateDoc, options);
-  // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+  const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
   res.send({ result, token });
 });
 
